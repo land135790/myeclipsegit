a982e70de7403818f98e8bb79547aa221665b52e
chensf <913279957@qq.com>
2019-07-25 19:43:36 +0800

    冲突测试

--- MyBatis_01_HelloWorld/src/com/atguigu/mybatis/dao/EmployeeMapper.java
+++ MyBatis_01_HelloWorld/src/com/atguigu/mybatis/dao/EmployeeMapper.java
@@ -2,7 +2,7 @@ package com.atguigu.mybatis.dao;
 
 import com.atguigu.mybatis.bean.Employee;
 
-// hello world
+// hello world!
 public interface EmployeeMapper {
 
 	public Employee getEmpById(Integer id);
